5a2d28e0840a7dfde3b67ce3f40d814b4397363a
Ivan Zivkovic <ivan.zivkovic0@hotmail.com>
2025-01-06 18:25:01 +0100

    spawning 1 thread per sample in the z direction

--- main.cu
+++ main.cu
@@ -108,45 +108,111 @@ __global__ void rand_init(curandState *rand_state)
     }
 }
 
-__global__ void render_init(int max_x, int max_y, curandState *rand_state) 
+__global__ void render_init(int max_x, int max_y, int ns, curandState *rand_state) 
 {
-    int i = threadIdx.x + blockIdx.x * blockDim.x;
-    int j = threadIdx.y + blockIdx.y * blockDim.y;
+    int tx = threadIdx.x;
+    int ty = threadIdx.y;
+
+    int i = tx + blockIdx.x * blockDim.x;
+    int j = ty + blockIdx.y * blockDim.y;
     if((i >= max_x) || (j >= max_y)) return;
     int pixel_index = j*max_x + i;
     // Original: Each thread gets same seed, a different sequence number, no offset
     // curand_init(1984, pixel_index, 0, &rand_state[pixel_index]);
     // BUGFIX, see Issue#2: Each thread gets different seed, same sequence for
     // performance improvement of about 2x!
-    //curand_init(1984+pixel_index, 0, 0, &rand_state[pixel_index]);
-    curand_init(333+pixel_index, 0, 0, &rand_state[pixel_index]);
+    curandState local_rand_state;
+    curand_init(1984+pixel_index, 0, 0, &local_rand_state);
+
+    // // To make sure &local_rand_state is in correct value in each sample
+    // rand_state[pixel_index * ns] = local_rand_state;
+    // for (int k = 1; k < ns; k++){
+    //     curand_uniform(&local_rand_state);
+    //     curand_uniform(&local_rand_state);
+    //     random_in_unit_disk(&local_rand_state);
+    //     random_in_unit_sphere(&local_rand_state);
+    //     rand_state[pixel_index*ns + k] = local_rand_state;
+    // }
+
+    // Temporary buffer to store random states
+    __shared__ curandState rand_buffer[8][8][10];
+
+    // Store the first random state directly
+    rand_buffer[tx][ty][0] = local_rand_state;
+    
+    // Process all subsequent random states in a temporary buffer
+    for (int k = 1; k < ns; k++) {
+        // Update the random state locally
+        curand_uniform(&local_rand_state);
+        curand_uniform(&local_rand_state);
+        random_in_unit_disk(&local_rand_state);
+        random_in_unit_sphere(&local_rand_state); // This one is problematic. Not all the sample will do this actually
+        rand_buffer[tx][ty][k]  = local_rand_state;
+    }
+
+    // Write all states back to global memory
+    for (int k = 0; k < ns; k++) {
+        rand_state[pixel_index*ns + k] = rand_buffer[tx][ty][k];
+    }
 }
+
 
 __global__ void render(vec3 *fb, int max_x, int max_y, int ns, const camera* cam, const World* world, curandState* rand_state) 
 {
     int i = threadIdx.x + blockIdx.x * blockDim.x;
     int j = threadIdx.y + blockIdx.y * blockDim.y;
-    if((i >= max_x) || (j >= max_y)) return;
-    int pixel_index = j*max_x + i;
-    curandState local_rand_state = rand_state[pixel_index];
-    vec3 col(0,0,0);
+    int s = threadIdx.z;
+    if((i >= max_x) || (j >= max_y) || (s >= ns)) return;
+    
 
-    for(int s=0; s < ns; s++) 
-    {
-        float u = float(i + curand_uniform(&local_rand_state)) / float(max_x);
-        float v = float(j + curand_uniform(&local_rand_state)) / float(max_y);
-
-        ray r = cam->get_ray(u, v, &local_rand_state);
-
-        col += color(r, world, &local_rand_state);  // bottleneck of this function
+     __shared__ vec3 shared_color[8][8]; // Have to modify this parameter
+    int tx = threadIdx.x;
+    int ty = threadIdx.y;
+    
+    if (s == 0) {
+        shared_color[ty][tx] = vec3(0.0, 0.0, 0.0);
     }
-    rand_state[pixel_index] = local_rand_state;
-    col /= float(ns);
-    col[0] = sqrt(col[0]);
-    col[1] = sqrt(col[1]);
-    col[2] = sqrt(col[2]);
-    fb[pixel_index] = col;    
+    __syncthreads();
+
+    int pixel_index = j*max_x + i;
+    curandState local_rand_state = rand_state[pixel_index*ns + s];
+
+ 
+    float u = float(i + curand_uniform(&local_rand_state)) / float(max_x);
+    float v = float(j + curand_uniform(&local_rand_state)) / float(max_y);
+    ray r = cam->get_ray(u, v, &local_rand_state);
+    vec3 sample_color = color(r, world, &local_rand_state);
+
+    // atomicAdd(&fb[pixel_index].e[0], sample_color.e[0]);
+    // atomicAdd(&fb[pixel_index].e[1], sample_color.e[1]);
+    // atomicAdd(&fb[pixel_index].e[2], sample_color.e[2]);
+
+    atomicAdd(&shared_color[ty][tx].e[0], sample_color.e[0]);
+    atomicAdd(&shared_color[ty][tx].e[1], sample_color.e[1]);
+    atomicAdd(&shared_color[ty][tx].e[2], sample_color.e[2]);
+    __syncthreads();
+
+    if (s == ns - 1) {
+        atomicAdd(&fb[pixel_index].e[0], shared_color[ty][tx].e[0]);
+        atomicAdd(&fb[pixel_index].e[1], shared_color[ty][tx].e[1]);
+        atomicAdd(&fb[pixel_index].e[2], shared_color[ty][tx].e[2]);
+    }
 }
+
+
+__global__ void normalize_colors(vec3 *fb, int max_x, int max_y, int ns) 
+{
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    int j = threadIdx.y + blockIdx.y * blockDim.y;
+    if ((i >= max_x) || (j >= max_y)) return;
+
+    int pixel_index = j * max_x + i;
+    fb[pixel_index] /= float(ns);
+    fb[pixel_index].e[0] = sqrt(fb[pixel_index].e[0]);
+    fb[pixel_index].e[1] = sqrt(fb[pixel_index].e[1]);
+    fb[pixel_index].e[2] = sqrt(fb[pixel_index].e[2]);
+}
+
 
 #define RND (curand_uniform(&local_rand_state))
 
@@ -288,7 +354,7 @@ int main()
 
     // allocate random state
     curandState *d_rand_state;
-    checkCudaErrors(cudaMalloc((void **)&d_rand_state, num_pixels*sizeof(curandState)));
+    checkCudaErrors(cudaMalloc((void **)&d_rand_state, ns*num_pixels*sizeof(curandState)));
 
     curandState *d_rand_state2;
     checkCudaErrors(cudaMalloc((void **)&d_rand_state2, 1*sizeof(curandState)));
@@ -319,13 +385,21 @@ int main()
 
     clock_t start, stop;
     start = clock();
+
     // Render our buffer
-    dim3 threads(tx,ty);
+    dim3 threads(tx,ty, ns);
     dim3 blocks((nx+tx-1)/tx, (ny+ty-1)/ty);
-    render_init<<<blocks, threads>>>(nx, ny, d_rand_state);
+    
+    render_init<<<blocks, threads>>>(nx, ny, ns, d_rand_state);
     checkCudaErrors(cudaGetLastError());
     checkCudaErrors(cudaDeviceSynchronize());
+
     render<<<blocks, threads>>>(fb, nx, ny,  ns, d_camera, d_world, d_rand_state);
+    checkCudaErrors(cudaGetLastError());
+    checkCudaErrors(cudaDeviceSynchronize());
+
+    threads = {tx, ty};
+    normalize_colors<<<blocks, threads>>>(fb, nx, ny, ns);
     checkCudaErrors(cudaGetLastError());
     checkCudaErrors(cudaDeviceSynchronize());
     
@@ -335,16 +409,16 @@ int main()
 
 
     // Output FB as Image
-    // std::cout << "P3\n" << nx << " " << ny << "\n255\n";
-    // for (int j = ny-1; j >= 0; j--) {
-    //     for (int i = 0; i < nx; i++) {
-    //         size_t pixel_index = j*nx + i;
-    //         int ir = int(255.99*fb[pixel_index].r());
-    //         int ig = int(255.99*fb[pixel_index].g());
-    //         int ib = int(255.99*fb[pixel_index].b());
-    //         std::cout << ir << " " << ig << " " << ib << "\n";
-    //     }
-    // }
+    std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+    for (int j = ny-1; j >= 0; j--) {
+        for (int i = 0; i < nx; i++) {
+            size_t pixel_index = j*nx + i;
+            int ir = int(255.99*fb[pixel_index].r());
+            int ig = int(255.99*fb[pixel_index].g());
+            int ib = int(255.99*fb[pixel_index].b());
+            std::cout << ir << " " << ig << " " << ib << "\n";
+        }
+    }
 
 
     // clean up
